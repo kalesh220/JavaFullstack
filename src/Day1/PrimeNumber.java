7db54efd368abaafdb0ea2f33d02742bdefc667a
KALESWAR <kaleswararao1990@gmail.com>
2024-01-26 12:11:09 +0530

    first commit

---
+++ src/Day1/PrimeNumber.java
@@ -0,0 +1,25 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int num = sc.nextInt();
+        int remainder;
+        int count = 0;
+        for (int i=1; i<=num; i++){
+            remainder = num % i;
+            if(remainder ==0){
+                count++;
+            }
+        }
+        if(count==2){
+            System.out.println("you have entered prime number :"+ num);
+        }else{
+            System.out.println("you have entered non-prime number :"+num);
+        }
+
+    }
+}
