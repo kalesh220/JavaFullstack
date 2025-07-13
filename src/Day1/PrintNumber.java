c888125f2594f30e27a7b6ffb3fe21c7cf4618ef
kalesh220 <kaleswararao1990@gmail.com>
2025-07-13 20:29:32 +0530

    this is my first commit

---
+++ src/Day1/PrintNumber.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class PrintNumber {
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("Numbers From 1 to 50 : " + i);
+        }
+    }
+}
